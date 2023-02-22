51ad0e632d395a2782ff0ab0ca2a681035b5f00e
Moritz Gottschling <moritz@giantswarm.io>
2023-02-22 07:57:02 +0100

    Add rule to check whether properties have exactly one type (#39)

---
+++ pkg/lint/rules/properties_must_have_one_type.go
@@ -0,0 +1,31 @@
+package rules
+
+import (
+	"fmt"
+
+	"github.com/giantswarm/schemalint/pkg/lint"
+	"github.com/giantswarm/schemalint/pkg/lint/utils"
+	"github.com/giantswarm/schemalint/pkg/schemautils"
+)
+
+type PropertiesMustHaveOneType struct{}
+
+func (r PropertiesMustHaveOneType) Verify(schema *schemautils.ExtendedSchema) lint.RuleResults {
+	ruleResults := &lint.RuleResults{}
+
+	callback := func(schema *schemautils.ExtendedSchema) {
+		if len(schema.Types) > 1 {
+			ruleResults.Add(fmt.Sprintf("Property '%s' has %d types, but must have exactly one.", schema.GetHumanReadableLocation(), len(schema.Types)))
+		}
+		if len(schema.Types) == 0 && schema.Ref == nil {
+			ruleResults.Add(fmt.Sprintf("Property '%s' must have exactly one type unless '$ref' is used.", schema.GetHumanReadableLocation()))
+		}
+	}
+
+	utils.RecurseProperties(schema, callback)
+	return *ruleResults
+}
+
+func (r PropertiesMustHaveOneType) GetSeverity() lint.Severity {
+	return lint.SeverityError
+}
